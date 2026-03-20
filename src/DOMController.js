4f151f256ac73fdce4d0216c707ff328e613b0f6
Kgodisho Mononyane <kgodisho.mononyane@gmail.com>
2026-03-20 12:26:47 +0200

    add modifyValues function and fill it with hardcoded values.
    create modifyValues function which changes the values of the info in the html. modified HTML to allow elements to be changed to hardcoded values.

--- src/DOMController.js
+++ src/DOMController.js
@@ -4,5 +4,33 @@
   form.addEventListener("submit", (e) => {
     e.preventDefault();
     console.log("submit clicked. It works");
+    modifyValues()
   });
 })();
+
+
+function modifyValues() {
+  const cityName = document.querySelector("#city-name");
+  cityName.textContent = "Cape Town";
+
+  const tempValue = document.querySelector("#temperature-value");
+  tempValue.textContent = "24 °c";
+
+  const weatherValue = document.querySelector("#weather-value");
+  weatherValue.textContent = "Sunny";
+
+  const weatherDescription = document.querySelector("#weather-description");
+  weatherDescription.textContent = "Clear blue skies";
+
+  const windSpeed = document.querySelector("#wind-speed");
+  windSpeed.textContent = `Wind speed: Fast af`;
+
+  const feelsLike = document.querySelector("#feels-like")
+  feelsLike.textContent = "Feels like: 20 deg";
+
+  const cloudCover = document.querySelector("#cloud-cover");
+  cloudCover.textContent = "Cloud cover: 49%";
+
+  const humidity = document.querySelector("#humidity");
+  humidity.textContent = "Humidity: 11%";
+}
